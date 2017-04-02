f6f0d9437dc1edb4a257f51515a98088ad881cbc
markusressel <markus9100@gmail.com>
2017-04-02 20:23:36 +0200

    added basic tests

---
+++ tests/basic_test.py
@@ -0,0 +1,55 @@
+import unittest
+
+from sunix_ledstrip_controller_client import LEDStripControllerClient
+from sunix_ledstrip_controller_client.controller import Controller
+
+
+class TestStringMethods(unittest.TestCase):
+    def create_api(self):
+        """
+        Creates the api and object
+        """
+
+        api = LEDStripControllerClient()
+        self.assertIsNotNone(api)
+
+    def create_controller(self):
+        """
+        Creates a controller using default values
+        :return: 
+        """
+        host = "192.168.2.53"
+
+        device = Controller(host)
+        self.assertIsNotNone(device)
+
+        self.assertEqual(device.get_host(), host)
+        self.assertEqual(device.get_port(), Controller.DEFAULT_PORT)
+        self.assertEqual(device.get_hardware_id(), None)
+        self.assertEqual(device.get_model(), None)
+        self.assertEqual(device.get_rgbww(), None)
+        self.assertEqual(device.get_brightness(), None)
+
+    def create_controller_custom(self):
+        """
+        Creates a controller using default values
+        :return: 
+        """
+        host = "192.168.2.123"
+        port = 12345
+        hardware_id = "7D89D6789VVBGDJOFDJ"
+        model = "The Best One Ever"
+
+        device = Controller(host, port, hardware_id, model)
+        self.assertIsNotNone(device)
+
+        self.assertEqual(device.get_host(), host)
+        self.assertEqual(device.get_port(), port)
+        self.assertEqual(device.get_hardware_id(), hardware_id)
+        self.assertEqual(device.get_model(), model)
+        self.assertEqual(device.get_rgbww(), None)
+        self.assertEqual(device.get_brightness(), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
