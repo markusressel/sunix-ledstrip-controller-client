0169ec0f6d6ab86f6f2f53d5a317d558a6aad043
markusressel <markus9100@gmail.com>
2017-04-02 20:03:48 +0200

    asci fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='sunix_ledstrip_controller_client',
     version='1.0.0',
-    description='A library for controlling the Sunix® RGB / RGBWWCW WiFi LED Strip controller',
+    description='A library for controlling the Sunix RGB / RGBWWCW WiFi LED Strip controller',
     license='GPLv3+',
     author='Markus Ressel',
     author_email='mail@markusressel.de',
