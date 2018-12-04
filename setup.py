26ab57c93962524a4253e87e52b72aed4ede9243
Markus Ressel <mail@markusressel.de>
2018-12-04 22:49:32 +0100

    v2.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 
 from setuptools import setup, find_packages
 
-VERSION_NUMBER = "2.0.3"
+VERSION_NUMBER = "2.0.4"
 
 GIT_BRANCH = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
 GIT_BRANCH = GIT_BRANCH.decode()  # convert to standard string
